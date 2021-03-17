5f73ec7c350aa1cf5224880c8a4559e0b3a7f80d
Shawn Leonard <sleonard@microsoft.com>
2021-03-17 14:19:35 -0600

    adding serilog, startup for configuration

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+
+[assembly: FunctionsStartup(typeof(Funcs_DataMovement.Startup))]
+
+namespace Funcs_DataMovement
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Registering Configurations (IOptions pattern)
+            var SAS = Environment.GetEnvironmentVariable("Serilog:SAS"); 
+            var AccountName = Environment.GetEnvironmentVariable("Serilog:AccountName"); 
+            var tableEndpoint = new Uri(Environment.GetEnvironmentVariable("Serilog:TableUri"));
+
+            // Registering Serilog provider
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.AzureTableStorage(SAS, AccountName, tableEndpoint, Serilog.Events.LogEventLevel.Information, storageTableName: "logs").Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Services.AddLogging(lb => lb.AddSerilog(logger));
+
+        }
+    }
+}
